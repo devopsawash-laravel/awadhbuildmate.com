f28d51c787472a20b383dd342842f6b826ac6372
devopsawash-laravel <devops.awash@gmail.com>
2026-05-24 16:17:09 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,9 +10,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SiteSeeder::class,
             BankSeeder::class,
             LabourSeeder::class,
-            SiteSeeder::class,
         ]);
     }
 }
