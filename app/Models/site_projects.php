5070496e4d8db1519c663261ab14546848ac20cc
Shivani <shivanimistry100@gmail.com>
2026-05-24 14:38:58 +0530

    fixed all case sensitive models

--- app/Models/site_projects.php
+++ app/Models/site_projects.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class site_projects extends Model
+class Site_projects extends Model
 {
     use HasFactory;
 }
